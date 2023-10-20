f12c32b693d8864ba7e74fba5b2939333e026b99
IgboPharaoh <solomoneze62@gmail.com>
2023-10-20 23:40:40 +0100

    add required types

--- src/types/pull_requests.ts
+++ src/types/pull_requests.ts
@@ -1,9 +1,13 @@
+import { Comment } from "./comments"
+
 export type PR = {
     totalComments: number
     daysOpened: number
     url: string
     repoUrl: string
     title: string
+    avatarUrl: string
+    project: { login: string; avatarUrl: string }
 }
 
 export type PullRequests = {
@@ -12,4 +16,23 @@ export type PullRequests = {
     openInactivePRs: PR[]
     closedPRs: PR[]
     closedPRsByOthers: PR[]
+}
+
+export type IssuesObject = {
+    ownIssueComments: Comment[]
+    longIssueComments: Comment[]
+    othersIssueComments: Comment[]
+}
+
+export type PRsObject = {
+    openPrs: PR[]
+    openInactivePrs: PR[]
+    closedPrs: PR[]
+    closedPrsByOthers: PR[]
+    mergedPrs: PR[]
+}
+
+export type Project = {
+    login: string
+    avatarUrl: string
 }
