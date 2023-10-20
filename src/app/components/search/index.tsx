37b200a60470df3eb1ac67cbbee836af6d0106c1
IgboPharaoh <solomoneze62@gmail.com>
2023-10-20 23:50:23 +0100

    remodify search page

--- src/app/components/search/index.tsx
+++ src/app/components/search/index.tsx
@@ -1,36 +1,25 @@
 "use client"
 
-import React, { useState } from "react"
-
-import {
-    getLongComments,
-    getOthersComments,
-    getOwnComments
-} from "@/helpers/get-comments"
-import { getPullRequests } from "@/helpers/get-pull-requests"
-import { Comment } from "@/types/comments"
-import { PR } from "@/types/pull_requests"
-
-import IssueCollapsibleComponent from "../issues/collapsible"
-import PullRequestCollapsibleComponent from "../pull-request/collapsible"
+import React, { useState, useEffect, useCallback } from "react"
 import Spinner from "../spinner"
 import { fetchIssues } from "./action"
+import { useRouter, useSearchParams } from "next/navigation"
 
 export default function Search() {
+    const Router = useRouter()
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
+    const searchParams = useSearchParams()
 
-    const [ownIssueComments, setOwnIssueComments] = useState<Comment[]>([])
-    const [longIssueComments, setLongIssueComments] = useState<Comment[]>([])
-    const [othersIssueComments, setOthersIssueComments] = useState<Comment[]>(
-        []
+    const createUrlQuery = useCallback(
+        (name: string, value: any) => {
+            const params = new URLSearchParams(searchParams)
+            params.set(name, value)
+
+            return `?${params.toString()}`
+        },
+        [searchParams]
     )
-
-    const [openPrs, setOpenPrs] = useState<PR[]>([])
-    const [openInactivePrs, setOpenInactivePrs] = useState<PR[]>([])
-    const [closedPrs, setClosedPrs] = useState<PR[]>([])
-    const [closedPrsByOthers, setClosedPrsByOthers] = useState<PR[]>([])
-    const [mergedPrs, setMergedPrs] = useState<PR[]>([])
 
     const fetchGithubIssues = async (formData: FormData) => {
         const username = formData.get("search")
@@ -39,14 +28,7 @@ export default function Search() {
         }
         setLoading(true)
         setError(null)
-        setOwnIssueComments([])
-        setLongIssueComments([])
-        setOthersIssueComments([])
-        setClosedPrs([])
-        setClosedPrsByOthers([])
-        setMergedPrs([])
-        setOpenPrs([])
-        setOpenInactivePrs([])
+
         const { issues, prs } = await fetchIssues({
             username: username as string
         })
@@ -58,37 +40,13 @@ export default function Search() {
             return
         }
 
-        const issue = getOwnComments({
-            data: issues.data,
-            username: username as string
-        })
-        const longIssue = getLongComments({
-            data: issues.data
-        })
-        const othersIssue = getOthersComments({
-            data: issues.data,
-            username: username as string
-        })
-        const {
-            closedPRs,
-            closedPRsByOthers,
-            mergedPRs,
-            openInactivePRs,
-            openPRs
-        } = getPullRequests({
-            data: prs.data,
-            username: username as string
-        })
-
-        setOwnIssueComments(issue)
-        setLongIssueComments(longIssue)
-        setOthersIssueComments(othersIssue)
-        setClosedPrs(closedPRs)
-        setClosedPrsByOthers(closedPRsByOthers)
-        setMergedPrs(mergedPRs)
-        setOpenInactivePrs(openInactivePRs)
-        setOpenPrs(openPRs)
+        Router.push("/result" + createUrlQuery("username", username))
     }
+
+    useEffect(() => {
+        Router.prefetch("/result")
+    }, [Router])
+
     return (
         <>
             <form
@@ -127,86 +85,6 @@ export default function Search() {
                     <p className="text-red-500 dark:text-red-400 text-center">
                         {error}
                     </p>
-                )}
-            </div>
-            <div className="flex flex-col justify-center items-center mt-4">
-                {ownIssueComments.length > 0 && (
-                    <div className="flex flex-col justify-center items-center mt-4">
-                        <IssueCollapsibleComponent
-                            type="own"
-                            issues={ownIssueComments}
-                        />
-                    </div>
-                )}
-            </div>
-            <div className="flex flex-col justify-center items-center mt-4">
-                {longIssueComments.length > 0 && (
-                    <div className="flex flex-col justify-center items-center mt-4">
-                        <IssueCollapsibleComponent
-                            type="long"
-                            issues={longIssueComments}
-                        />
-                    </div>
-                )}
-            </div>
-            <div className="flex flex-col justify-center items-center mt-4">
-                {othersIssueComments.length > 0 && (
-                    <div className="flex flex-col justify-center items-center mt-4">
-                        <IssueCollapsibleComponent
-                            type="others"
-                            issues={othersIssueComments}
-                        />
-                    </div>
-                )}
-            </div>
-            <div className="flex flex-col justify-center items-center mt-4">
-                {openPrs.length > 0 && (
-                    <div className="flex flex-col justify-center items-center mt-4">
-                        <PullRequestCollapsibleComponent
-                            type="open"
-                            pullRequests={openPrs}
-                        />
-                    </div>
-                )}
-            </div>
-            <div className="flex flex-col justify-center items-center mt-4">
-                {openInactivePrs.length > 0 && (
-                    <div className="flex flex-col justify-center items-center mt-4">
-                        <PullRequestCollapsibleComponent
-                            type="inactive"
-                            pullRequests={openInactivePrs}
-                        />
-                    </div>
-                )}
-            </div>
-            <div className="flex flex-col justify-center items-center mt-4">
-                {mergedPrs.length > 0 && (
-                    <div className="flex flex-col justify-center items-center mt-4">
-                        <PullRequestCollapsibleComponent
-                            type="merged"
-                            pullRequests={mergedPrs}
-                        />
-                    </div>
-                )}
-            </div>
-            <div className="flex flex-col justify-center items-center mt-4">
-                {closedPrs.length > 0 && (
-                    <div className="flex flex-col justify-center items-center mt-4">
-                        <PullRequestCollapsibleComponent
-                            type="closed"
-                            pullRequests={closedPrs}
-                        />
-                    </div>
-                )}
-            </div>
-            <div className="flex flex-col justify-center items-center mt-4">
-                {closedPrsByOthers.length > 0 && (
-                    <div className="flex flex-col justify-center items-center mt-4">
-                        <PullRequestCollapsibleComponent
-                            type="closed by others"
-                            pullRequests={closedPrsByOthers}
-                        />
-                    </div>
                 )}
             </div>
         </>
