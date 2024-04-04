3cb9c7c8995cb0c75c56f4ca8b5609fab513e86e
Solomon eze <87058633+IgboPharaoh@users.noreply.github.com>
2024-04-04 15:46:50 +0100

    Fix: Null issue comments and markdown rendering (#98)
    * intall necessary dependencies
    
    * add markdown wrapper to issue comment body
    
    * fix issue comment returning empty values for next year

---
+++ src/app/components/wrapper/MarkdownWrapper.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import ReactMarkdown from "react-markdown"
+import "../../globals.css"
+
+export const MarkdownWrapper = ({
+    summary,
+    className
+}: {
+    summary: string
+    className?: string | null | undefined
+}) => {
+    return (
+        <ReactMarkdown className={`markdownStyles ${className}`}>
+            {summary}
+        </ReactMarkdown>
+    )
+}
